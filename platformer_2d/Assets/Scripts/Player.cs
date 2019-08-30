7aa48312225d552720bd4f5f72701e862af3fb99
daisyx <daisy1xu4@gmail.com>
2019-08-29 23:27:33 -0700

    got simple jump physics on cat

--- platformer_2d/Assets/Scripts/Player.cs
+++ platformer_2d/Assets/Scripts/Player.cs
@@ -3,7 +3,10 @@
 public class Player : MonoBehaviour {
     public float MoveSpeed;
 
+    [SerializeField] private LayerMask platformLayerMask; // which layer want to hit with raycast
+
     private Rigidbody2D myRigidBody;
+    private BoxCollider2D boxCollider2d;
     private Animator anim;
 
     private bool jump;
@@ -12,31 +15,49 @@ public class Player : MonoBehaviour {
     void Start() {
         myRigidBody = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        boxCollider2d = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetButtonDown("Jump")) {
-            jump = true;
+        if (isGrounded() && Input.GetButtonDown("Jump")) {
+            jump = false;
+            myRigidBody.velocity = Vector2.up * MoveSpeed;
         }
+
+        handleMovement();
+        handleAnimation();
+
     }
 
-    private void FixedUpdate() {
+    private bool isGrounded() {
+        RaycastHit2D rayCast2d = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down, 0.1f, platformLayerMask);
+        return rayCast2d;
+    }
+
+    private void handleMovement() {
         float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = 0f;
 
-        if (jump) {
-            moveY = 20f;
-            jump = false;
-        }
+        myRigidBody.velocity = new Vector2(moveX * MoveSpeed, myRigidBody.velocity.y);
+    }
 
-        myRigidBody.velocity = new Vector2(moveX, moveY) * MoveSpeed;
+    private void handleAnimation() {
+        float moveX = myRigidBody.velocity.x;
 
         anim.SetFloat("xMove", moveX);
-        anim.SetBool("isMoving", moveX > 0.1 || moveX < -0.1);
 
         if (moveX > 0.1 || moveX < -0.1) {
             anim.SetFloat("lastXMove", moveX);
+        }
+
+        if (isGrounded()) {
+            // is on the ground
+            anim.SetBool("isJumping", false);
+            anim.SetBool("isMoving", moveX > 0.1 || moveX < -0.1);
+        } else {
+            // is in the air
+            anim.SetBool("isMoving", false);
+            anim.SetBool("isJumping", true);
         }
     }
 }
