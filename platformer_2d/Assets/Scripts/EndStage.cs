e5209bd2506cb74232001874cab1c6f0f7005c8f
daisyx <daisy1xu4@gmail.com>
2020-11-21 16:38:30 -0800

    finish ending animation

--- platformer_2d/Assets/Scripts/EndStage.cs
+++ platformer_2d/Assets/Scripts/EndStage.cs
@@ -7,7 +7,7 @@ public class EndStage : MonoBehaviour {
 
     private Animator anim;
 
-    private float animationTime = 1.5f;
+    private float animationTime = 5.5f;
 
     // Start is called before the first frame update
     void Start() {
